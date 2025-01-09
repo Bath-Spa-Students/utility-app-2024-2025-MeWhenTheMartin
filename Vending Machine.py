fc891b1e7579d0332bf869f03c333be838782551
MartinEdu <MeWhenTheMartin@users.noreply.github.com>
2025-01-09 14:14:58 +0400

    Withdraw function
    Withdrawing money function complete, refined the coding formats.

--- Vending Machine.py
+++ Vending Machine.py
@@ -1,26 +1,29 @@
 ### Vending Machine
+print("Please wait...")
+
+#-----------------------------------------------------------------------------------------------------------#
+###                              ### ----- GLOBAL VARIABLES ----- ###                                     ###
+#-----------------------------------------------------------------------------------------------------------#
 
 '''
 NOTES:
 Variables with Capitals should be reserved for Global Variables
 '''
-print("Please wait...")
 
-## Global Variables
-
-# User Values
+# User Values #
 Cash = 0
 Money = Cash
+Current = "Unassigned"
+ListCurrent = "Currently idle", "Browsing the store", "Purchasing items", "At the bank"
 
-## HUD
+## HUD ##
 
-# Settings
-MXLenI = 15
-MXLenP = 9
+# Settings #
+MXLenI, MXLenP = 15, 9
 MaxLen = MXLenI
 space = 0
 
-# HUD Elements
+# HUD Elements #
 Blanks = " "
 NewPage = "\n\n\n"
 NewPage = NewPage*15
@@ -29,9 +32,11 @@ Divider = (f"""
 {Divider*125}
 """)
 
-# Items
+# Items #
 Type = ["Drink", "Snack"]
-# Maximum Characters for Items is [15]
+
+# Maximum Characters for naming Items is [15]
+
 Menus1 = { # Drinks
         "Regular Water"   :50,
         "Mineral Water"   :150,
@@ -56,36 +61,40 @@ Menus2 = { # Snacks
 ItemsP2 = list(Menus2.keys())
 PricesP2 = list(Menus2.values())
 
-MenuNo = 0
-Active = 1
+MenuNo, Active, Action = 0, 1, 0
+LastAction = 0
+PrintedHUD = 1
 
-Action = 0
-# 0 = Initiate Console
-# 1 = Browsing Shop
-# 2 = Buying Items
-# 3 = Deposit/Withdraw Cash
+## Functions ##
 
-## Functions
+# Menu/HUD #
 
-# Menu/HUD
+def HUD(type, inputs): # For displaying formats
 
-def HUD(type, inputs): # For Displaying Global Variables
-    # 1 = CURRENT ACTIONS
-    # 2 = INTERACTIONS
-    # 3 = INFORMATION
-    if type == 1:
+    #--------NOTES--------#
+    # 1 =                 #
+    # 2 = INTERACTIONS    #
+    # 3 = INFORMATION     #
+    #---------------------#
+
+    Current = ListCurrent[Action] # Update
+
+    if type == 1: # Menu
         print(type)
-    if type == 2:
+    if type == 2: # Bank
         print(type)
-    if type == 3:
-        print(f"{Divider}Money:{Cash}\nBrowsing the shop...{Divider}Vending Machine{Divider}", end=''), printStoreMenu(inputs)
-        print(f"{Divider}8[BUY] 9[DEPOSIT] 99[Back]\nItems Dispensed:")
+    if type == 3: # Store
+        print(f"{Divider}Money:{Cash}\n{Current}...{Divider}Vending Machine{Divider}", end=''), printStoreMenu(inputs)
+        if Action == 2:
+            print(f"{Divider}99[Back]\nItems Dispensed:")
+        else:
+            print(f"{Divider}8[Buy] 9[Deposit] 99[Back]\nItems Dispensed:")
     else:
         print("Function 'HUD' did not return from int 1-3, You may have entered out of value or a str.")
 
 default_stock = 10
 
-def printItemMenu(no,item,price,stock,price_,stock_):
+def printItemMenu(no,item,price,stock,price_,stock_): # from printStoreMenu()
     if Action == 2:
         print(f"{no}[{item}{price_}]   {price}{stock_}{stock}")
     else:
@@ -94,15 +103,15 @@ def printItemMenu(no,item,price,stock,price_,stock_):
 def printStoreMenu(Num):
     print(f" Item{Blanks*16}Value    Stock")
     num, go = 0, 0
-    # Page Number
-    if   Num == 1:
+
+    if   Num == 1:     # Page Numbers
         ItemsNum, PricesNum, go = ItemsP1, PricesP1, 1
     elif Num == 2:
         ItemsNum, PricesNum, go = ItemsP2, PricesP2, 1
     else:
         print('Menu does not exist...'), go == 0
-    # Print Menu
-    if go == 1:
+
+    if go == 1:     # Print Menu
         for item, price in zip(ItemsNum, PricesNum):
             I = MXLenI - len(item)
             P = MXLenP - len(str(price))
@@ -110,26 +119,91 @@ def printStoreMenu(Num):
             P = Blanks * P
             printItemMenu(num,item,PricesNum[num],default_stock,I,P)
             num += 1
-        
-    print(f"\n6[Back] Page {Num}/2 7[Next]{Divider}")
+
+    if Action == 2:   
+        print(f"\n6[Back] Page {Num}/2 7[Next]{Divider}")
+    else:
+        print(f"\n1[Back] Page {Num}/2 2[Next]{Divider}")
 
 def Interact():
     print("Interact")
 
-# --- END OF VARIABLES ---
+
+#-----------------------------------------------------------------------------------------------------------#
+###                              ### ----- END OF VARIABLES ----- ###                                     ###
+#-----------------------------------------------------------------------------------------------------------#
+
 
 # Boot/Menu
-print(NewPage,Divider,end='')
-print(f'''Browsing the store...{Divider}What would you like to do today?
+def DasBoot():
+    print(NewPage,Divider,end='')
+    Current = ListCurrent[Action]
+    print(f'''{Current}...{Divider}What would you like to do today?
 1[Shop] 2[Withdraw/Deposit] 99[Leave]''')
 
-## CYCLE
+DasBoot()
 
+## CYCLE
 while True: 
     try: 
-        Input = int(input("Input:"))
-        print(NewPage)
-        HUD(3,Input)
+        if PrintedHUD == 1:
+            Input = int(input("Input:"))
+        PrintedHUD = 1
+
+        print(f"{Divider}\nDEBUG:")
+        print(f"Var [Action]:{Action}")
+        print(f"Var [Input]:{Input}")
+        print(Divider)
+
+        #-----------NOTES-----------#
+        # 0 = Initiate Console      #
+        # 1 = Browsing Shop         #
+        # 2 = Buying Items          #
+        # 3 = Deposit/Withdraw Cash #
+        #---------------------------#
+
+        if Action == 0:# Initiaton
+
+            if Input == 1:
+                Action = 1
+            if Input == 2:
+                Action = 3
+                print("Withdraw/Deposit")
+            if Input == 99:
+                print("Terminal shut down...")
+                break
+            if not Input == 1 or 2 or 99:
+                print("Script Broke")
+
+            print(NewPage,Divider,end='')
+            print(f'''Browsing the store...{Divider}What would you like to do today?\n1[Shop] 2[Withdraw/Deposit] 99[Leave]''')
+
+        if Action == 1:# Browser
+
+            if Input == 8:
+                Action = 2
+                HUD(3,1)
+            elif Input == 9:
+                Action = 3
+                HUD(3,1)
+            elif Input == 1 or 2:
+                HUD(3,Input)
+            elif not Input == 1 or 2 or 8 or 9:
+                print("Input unknown")
+
+        if Action == 2:# Buying 
+            print(f"Var [Action]:{Action} 'You're in the buy menu.'")
+            HUD(3,1)
+
+        if Action == 3:# Deposit/Withdraw
+            print(f"{NewPage}Please enter amount to withdraw... (Recomended:200)\nCurrent Cash:{Cash}")
+            Amount = Cash + int(input("Amount:"))
+            Cash, Action = Amount, LastAction
+            PrintedHUD = 0
+        
+        LastAction = Action
     
     except ValueError:
         print("Invalid input! Please enter a valid integer.")
+
+# Quick tests
